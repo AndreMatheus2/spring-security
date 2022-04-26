732b1fc7b2666d294bd05bc1a982ec51c240255d
Andre Matheus <am_arao@hotmail.com>
2022-04-26 14:12:58 -0300

    JpaRepository e Query filtrando no banco de dados

--- src/main/java/dio/spring/security/repository/UserRepository.java
+++ src/main/java/dio/spring/security/repository/UserRepository.java
@@ -3,6 +3,10 @@ package dio.spring.security.repository;
 import dio.spring.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User Integer> {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+public interface UserRepository extends JpaRepository<User, Integer> {
+        @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+        public User findByUsername(@Param ("username") String username);
 }
